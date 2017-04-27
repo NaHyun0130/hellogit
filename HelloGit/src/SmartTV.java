c50637f918fbdb6aa2dd9ceb2a35321a8f16c112
wndus <a01077956536@gmail.com>
2017-04-27 12:43:53 +0900

    xxx

--- HelloGit/src/SmartTV.java
+++ HelloGit/src/SmartTV.java
@@ -1,4 +1,28 @@
 
 public class SmartTV {
 
+
+	private int channel;
+
+	public int getChannel() {
+		return channel;
+	}
+
+	public void setChannel(int channel) {
+		this.channel = channel;
+	}
+	@Override
+	public String getName(){
+		return "SmartTV";
+	}
+	
+	public double maxSpeed(){
+		return 10.0;
+	}
+	
+	public boolean isConnected(){
+		return false;
+	}
+	
+	
 }
