17d76b560166d808cd7342c35e0028d7ae713436
wndus <a01077956536@gmail.com>
2017-04-27 12:30:13 +0900

    s

--- HelloGit/src/DataStorage.java
+++ HelloGit/src/DataStorage.java
@@ -1,16 +1,8 @@
 public interface DataStorage {
-<<<<<<< HEAD
-public double getFreeCapacity();
-public void format();
-=======
 
-	double getFreeCapacity(){
-		return capacity;
-	}
-	
-	void format(){
-		
-	}
->>>>>>> branch 'master' of https://github.com/NaHyun0130/hellogit.git
+
+	public double getCapacity();
+	public void format();
+
 }
 
