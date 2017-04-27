b338e85dcd7adebdfed49e8e33e65794d5de704e
NaHyun <maengna0130@naver.com>
2017-04-27 11:16:09 +0900

    hahahah

---
+++ HelloGit/src/net/NaHyun0130/hellogit/HelloMiddleEarth.java
@@ -0,0 +1,17 @@
+package net.NaHyun0130.hellogit;
+
+public class HelloMiddleEarth {
+
+	public static void main(String[] args) {
+		
+		String[] names = {"Bilbo", "Frodo", "Aragorn", "Legolas", "Gandalf"};
+	
+		
+		//print greetings
+		for(int i = 0; i < names.length; i++)
+		{
+			System.out.format("Hello, %s!\n", names[i]);
+		}
+
+	}
+}
