3c35f445a4eb5addda16990e3113b4044e78b128
최혜지 <@DESKTOP-3NTISKD>
2017-04-27 12:59:48 +0900

    얘드라 내가 수정해따~~

--- HelloGit/src/Laptop.java
+++ HelloGit/src/Laptop.java
@@ -1,49 +1,55 @@
+public class Laptop extends Product implements DataStorage {
+   
+   
+   private double totalCapacity;
+   private double usedCapacity;
 
-public class Laptop {
+   
+   public double getFreeCapacity() {
+      return 5.0;
+   }
+   
+   public void format() {
+      System.out.println("product format");
+   }
+   
+   public boolean isConnected() {
+      return true;
+   }
+   
+   public double maxSpeed() {
+      return 10.0;
+      
+   }
+   
+   public double getTotalCapacity() {
+      return totalCapacity;
+   }
 
-	private double totalCapacity;
-	private double usedCapacity;
+   public void setTotalCapacity(double totalCapacity) {
+      this.totalCapacity = totalCapacity;
+   }
 
-	
-	public double getFreeCapacity() {
-		return 5.0;
-	}
-	
-	public void format() {
-		System.out.println("product format");
-	}
-	
-	public boolean isConnected() {
-		return true;
-	}
-	
-	public double maxSpeed() {
-		return 10.0;
-		
-	}
-	
-	public double getTotalCapacity() {
-		return totalCapacity;
-	}
+   public double getUsedCapacity() {
+      return usedCapacity;
+   }
 
-	public void setTotalCapacity(double totalCapacity) {
-		this.totalCapacity = totalCapacity;
-	}
+   public void setUsedCapacity(double usedCapacity) {
+      this.usedCapacity = usedCapacity;
+   }
+   @Override
+   public String getName(){
+      return "Laptop";
+   }
 
-	public double getUsedCapacity() {
-		return usedCapacity;
-	}
+   @Override
+   public double getCapacity() {
+   
+      return 1.0;
+   }
+   
 
-	public void setUsedCapacity(double usedCapacity) {
-		this.usedCapacity = usedCapacity;
-	}
-	@Override
-	public String getName(){
-		return "Laptop";
-	}
-	
-
-	
-	
+   
+   
 
 }
