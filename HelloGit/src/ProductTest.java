293c3b160536dc39e64dd0a19dc6900a6a407d3f
최혜지 <@DESKTOP-3NTISKD>
2017-04-27 13:22:47 +0900

    sssss

--- HelloGit/src/ProductTest.java
+++ HelloGit/src/ProductTest.java
@@ -4,9 +4,9 @@ public class ProductTest {
 	ProductTest p=new ProductTest();
 	
 	
-	Product p1=new SmartTV();
-	Product p2=new Laptop();
-	Product p3=new Toaster();
+	SmartTV p1 = new SmartTV();
+	Laptop p2 = new Laptop();
+	Toaster p3 = new Toaster();
 	
 	p.testCooker(p3);
 	p.testDataStorage(p2);
@@ -14,21 +14,21 @@ public class ProductTest {
 	p.testProduct(p1);
 
 	}
-public void testProduct(Product p){
-	System.out.println(p.getPrice);
-}
-public void testDataStorage(DataStorage d){
-	System.out.println(d.getFreeCapacity);
-
-}
-public void testNetworked(Networked n){
-	System.out.println(n.maxSpeed);
-
-}
-public void testCooker(Cooker c){	
-	System.out.println(c.prepareFood);
-
-}
+	public void testProduct(Product p){
+		System.out.println(p.getPrice());
+	}
+	public void testDataStorage(DataStorage d){
+		System.out.println(d.getFreeCapacity());
+	
+	}
+	public void testNetworked(Laptop p2){
+		System.out.println(p2.maxSpeed());
+	
+	}
+	public void testCooker(Cooker c){	
+		System.out.println(c.prepareFood());
+	
+	}
 
 
 }
